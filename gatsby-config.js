d256267a877b397bab1c03182f69ba1cb02d035a
Jorge <jotagmzn@gmail.com>
2019-08-13 23:11:44 +0200

    Update yml

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,10 +7,24 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/static/img`,
+        name: 'uploads',
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages',
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/img`,
+        name: 'images',
       },
     },
     `gatsby-transformer-sharp`,
@@ -18,17 +32,22 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Jotagep's Website`,
+        short_name: `Jotagep`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        display: `standalone`,
+        icon: `src/img/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
+    {
+      resolve: 'gatsby-plugin-netlify-cms',
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
+    'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
 }
---
+++ src/cms/cms.js
@@ -0,0 +1,28 @@
+/**
+ * The default export of `netlify-cms-app` is an object with all of the Netlify CMS
+ * extension registration methods, such as `registerWidget` and
+ * `registerPreviewTemplate`.
+ */
+import CMS from "netlify-cms-app"
+
+/**
+ * Any imported styles will automatically be applied to the editor preview
+ * pane, there is no need to use `registerPreviewStyle` for imported styles.
+ * All of the example imports below would result in styles being applied to the
+ * preview pane.
+ */
+// import "module-that-imports-styles.js"
+// import "styles.scss"
+// import "../other-styles.css"
+
+/**
+ * Let's say you've created widget and preview components for a custom image
+ * gallery widget in separate files:
+ */
+// import ImageGalleryWidget from "./image-gallery-widget.js"
+// import ImageGalleryPreview from "./image-gallery-preview.js"
+
+/**
+ * Register the imported widget:
+ */
+// CMS.registerWidget(`image-gallery`, ImageGalleryWidget, ImageGalleryPreview)
