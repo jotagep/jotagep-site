e0189850015df20c887cf05bb0e5d63bd072b3b8
Jorge <jotagmzn@gmail.com>
2020-06-01 13:47:24 +0200

    Update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,13 +35,6 @@ module.exports = {
       }
     },
     {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/content/projects`,
-        name: 'projects'
-      }
-    },
-    {
       resolve: "@narative/gatsby-theme-novela",
       options: {
         contentPosts: "content/posts",
